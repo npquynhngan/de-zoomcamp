e799ab5156b408aaf66c89e338e5765e6b3fb1d2
npquynhngan <117847566+npquynhngan@users.noreply.github.com>
2026-02-09 14:25:52 +0000

    fix: use click to create CLI params

--- docker-sql/pipeline/ingest_data.py
+++ docker-sql/pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm import tqdm
@@ -29,19 +30,17 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunk_size = 100000
+@click.command()
+@click.option("--pg-user", default="root", show_default=True)
+@click.option("--pg-password", default="root", show_default=True)
+@click.option("--pg-host", default="localhost", show_default=True)
+@click.option("--pg-port", default="5432", show_default=True)
+@click.option("--pg-db", default="ny_taxi", show_default=True)
+@click.option("--year", default=2021, type=int, show_default=True)
+@click.option("--month", default=1, type=int, show_default=True)
+@click.option("--target-table", default="yellow_taxi_data", show_default=True)
+@click.option("--chunk-size", default=100000, type=int, show_default=True)
+def run(pg_user, pg_password, pg_host, pg_port, pg_db, year, month, target_table, chunk_size):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
